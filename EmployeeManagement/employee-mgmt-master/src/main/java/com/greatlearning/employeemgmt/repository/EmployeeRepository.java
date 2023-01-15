266755a204646e3edaa06c9157ec020706616868
yashas911 <82862777+yashas911@users.noreply.github.com>
2023-01-15 19:34:31 +0530

    Delete EmployeeManagement/employee-mgmt-master directory

--- EmployeeManagement/employee-mgmt-master/src/main/java/com/greatlearning/employeemgmt/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.employeemgmt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.employeemgmt.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-
-}
